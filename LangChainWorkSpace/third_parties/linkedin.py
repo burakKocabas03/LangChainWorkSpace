c6c0e210d43ceb4fbc0d9283d36dde1b6ac30e85
burakKocabas03 <05220000303@ogrenci.ege.edu.tr>
2025-06-23 11:32:06 +0300

    Delete LangChainWorkSpace directory

--- LangChainWorkSpace/third_parties/linkedin.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-from dotenv import load_dotenv
-load_dotenv()
-
-
-
-#def scrape_linkedin_profile(linkedin_profile_url: str,mock: bool=False):
-
--- LangChainWorkSpace/tools/tools.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-def get_profile_url_tavily(name:str):
-    search=TavilySearchResults()
-    res =search.run(f"{name}")
-    return res
